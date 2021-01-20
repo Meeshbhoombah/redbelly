7d9aa26d8a40ab2409b345ac097f27a28fdd218b
Rohan Mishra <rohanmishra2017@gmail.com>
2021-01-19 18:17:36 -0800

    Started game module, removing build warnings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![warn(clippy::all)]
+#![allow(dead_code, unused_imports)]
 
 extern crate rand;
 extern crate piston_window;
 
 mod draw;
 mod snake;
+mod game;
 
 fn main() {
     println!("Hello, world!");
